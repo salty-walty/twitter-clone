a2ddecc2242e8a7bb1804887ab09553b7d4e308f
salty-walty <waltonsj3@gmail.com>
2018-12-03 12:16:11 -0500

    added tweets, feed, users, following, feed, and more

---
+++ app/models/tweet.rb
@@ -0,0 +1,6 @@
+class Tweet < ApplicationRecord
+	belongs_to :user
+
+	validates :message, presence: true
+	validates :message, length: {maximum: 140, too_long: "A tweet can only be 140 characters max"}
+end
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+
+  has_many :tweets
+
+  mount_uploader :avatar, AvatarUploader
+
+  serialize :following, Array
+  
+end
